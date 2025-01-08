012334d054dd74f53276015c571b42e0808c13b5
zzmarques <walacemarqs@gmail.com>
2025-01-08 13:26:31 -0300

    finalizando componente ConfirmOrder

--- src/components/ConfirmOrder/index.jsx
+++ src/components/ConfirmOrder/index.jsx
@@ -4,16 +4,36 @@ import '../../sass/components/ConfirmOrder.scss';
 
 
 const ConfirmOrder = ({ priceTotal, infos }) => {
+    
+    const thumb = infos.map(el => {
+        for (const e of data) {
+
+            if(e.name === el.name) {
+                return e.image.thumbnail;
+            }
+        }
+    });
+
+    const orderPriceTotal = infos.map(el => {
+        if(el.qtde > 1) {
+            return el.qtde * el.price
+        } else {
+            return el.price
+        }
+    });
 
     const handleConfirm = (btn) => {
         const containerOrder = btn.closest('.confirm-order');
         containerOrder.remove();
-        window.location.reload()
+        window.location.reload();
     }
 
 
     return (
-        <div className="confirm-order">
+        <>
+            <div className="fundoEscuro"></div>
+            <div className="confirm-order">
+            
 
             <div className="container-title">
                 <GoCheckCircle className="check"/>
@@ -23,70 +43,39 @@ const ConfirmOrder = ({ priceTotal, infos }) => {
 
             <div className="card-order">
 
-                <div className="container-orders">
-
-                    <div className="container-order">
-                        <figure className="order-photo">
-                            <img src={data[0].image.thumbnail} alt="" />
-
-                            <div className="infos-order">
-                                <span className="order-name">Classic Tiramisu</span>
-
-                                <div className="price-x-qtde">
-                                    <span className="qtde-order">1x</span>
-                                    <span className="price-order">@ $5.50</span>
-                                </div>
-                            </div>
-                        </figure>
-
-                        <div className="order-pirce-total">
-                            <span>$5.50</span>
+                {
+                    infos.length >= 0 ? (
+                        <div className="container-cards">
+                            {
+                                infos.map((el, i) => (
+                                    <div className="container-orders" key={i}>
+                                        <div className="container-order">
+                                            <figure className="order-photo">
+                                                <img src={thumb[i]} alt="" />
+                    
+                                                <div className="infos-order">
+                                                    <span className="order-name">{el.name}</span>
+                    
+                                                    <div className="price-x-qtde">
+                                                        <span className="qtde-order">{el.qtde}x</span>
+                                                        <span className="price-order">@ ${el.price}</span>
+                                                    </div>
+                                                </div>
+                                            </figure>
+                
+                                            <div className="order-pirce-total">
+                                                <span>${orderPriceTotal[i]}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
                         </div>
-                    </div>
-
-                </div>
-
-                <div className="container-orders">
-                    <div className="container-order">
-                        <figure className="order-photo">
-                            <img src={data[0].image.thumbnail} alt="" />
-
-                            <div className="infos-order">
-                                <span className="order-name">Vanilla Bean Crème Brûlée</span>
-
-                                <div className="price-x-qtde">
-                                    <span className="qtde-order">4x</span>
-                                    <span className="price-order">@ $7.00</span>
-                                </div>
-                            </div>
-                        </figure>
-
-                        <div className="order-pirce-total">
-                            <span>$28.00</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="container-orders">
-                    <div className="container-order">
-                        <figure className="order-photo">
-                            <img src={data[0].image.thumbnail} alt="" />
-
-                            <div className="infos-order">
-                                <span className="order-name">Vanilla Panna Cotta</span>
-
-                                <div className="price-x-qtde">
-                                    <span className="qtde-order">2x</span>
-                                    <span className="price-order">@ $6.50</span>
-                                </div>
-                            </div>
-                        </figure>
-
-                        <div className="order-pirce-total">
-                            <span>$13.00</span>
-                        </div>
-                    </div>
-                </div>
+                    ) : (
+                        <p>Error</p>
+                    )
+                
+                }
 
                 <div className="container-price-final">
                         <span className="txt-order">Order Total</span>
@@ -99,6 +88,8 @@ const ConfirmOrder = ({ priceTotal, infos }) => {
                     Start New Order
             </button>
         </div>
+        </>
+        
     )
 }
 
